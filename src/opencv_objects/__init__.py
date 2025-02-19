a2b0cc1fb436aca2e870f8f9a1ddb377d2ca79c3
張議隆 <nick900828@gmail.com>
2025-02-19 14:22:18 +0800

    CHORE: Remove ArUco Related codes

--- src/opencv_objects/__init__.py
+++ src/opencv_objects/__init__.py
@@ -3,15 +3,13 @@ This module initializes the OpenCV objects package.
 It imports the ArUcoDetector, EpipolarLineDetector, and ChessboardCalibrator classes from their respective modules and
 sets the __all__ variable to include all three classes.
 Classes:
-    ArUcoDetector: A class for detecting ArUco markers.
     EpipolarLineDetector: A class for detecting epipolar lines.
     ChessboardCalibrator: A class for calibrating stereo cameras.
 __all__:
     List of public objects of that module, as interpreted by import *.
 """
 
-from .aruco_detector import ArUcoDetector
 from .epipolar_line_detector import EpipolarLineDetector
 from .chessboard_calibration import ChessboardCalibrator
 
-__all__ = ["ArUcoDetector", "EpipolarLineDetector", "ChessboardCalibrator"]
+__all__ = ["EpipolarLineDetector", "ChessboardCalibrator"]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -9,8 +9,8 @@ Imports:
     load_images_from_directory (from .file_utils): Function to load images from a directory.
     draw_lines (from .display_utils): Function to draw lines on images.
     apply_colormap (from .display_utils): Function to apply colormap to depth images.
-    draw_aruco_rectangle (from .display_utils): Function to draw a rectangle around ArUco markers.
-    update_aruco_info (from .display_utils): Function to update ArUco marker information.
+    save_setup_info (from .file_utils): Function to save setup information.
+    load_setup_info (from .file_utils): Function to load setup information.
 __all__:
     List of public objects of that module, as interpreted by import *.
     - 'get_starting_index'
@@ -26,9 +26,9 @@ __all__:
 """
 
 from .file_utils import get_starting_index, parse_yaml_config, setup_directories, setup_logging, \
-    save_images, load_images_from_directory
-from .display_utils import draw_lines, apply_colormap, draw_aruco_rectangle, update_aruco_info
+    save_images, load_images_from_directory, save_setup_info, load_setup_info
+from .display_utils import draw_lines, apply_colormap
 
 __all__ = ['get_starting_index', 'parse_yaml_config', 'setup_directories', 'setup_logging',
-           'save_images', 'load_images_from_directory',
-           'draw_lines', 'apply_colormap', 'draw_aruco_rectangle', 'update_aruco_info']
+           'save_images', 'load_images_from_directory', 'save_setup_info', 'load_setup_info',
+           'draw_lines', 'apply_colormap']
