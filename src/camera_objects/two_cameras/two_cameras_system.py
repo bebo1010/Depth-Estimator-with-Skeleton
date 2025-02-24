65cd49e34006f3fae663c1feb5717ff80942f713
張議隆 <nick900828@gmail.com>
2025-02-24 11:50:12 +0800

    FEAT: Add interface to get RGB images

--- src/camera_objects/two_cameras/two_cameras_system.py
+++ src/camera_objects/two_cameras/two_cameras_system.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from typing import Tuple
 
 import numpy as np
+import cv2
 
 class TwoCamerasSystem(ABC):
     """
@@ -18,6 +19,7 @@ class TwoCamerasSystem(ABC):
         get_height() -> int
         release() -> bool
     """
+
     def __init__(self) -> None:
         """
         Initialize two cameras system.
@@ -28,6 +30,7 @@ class TwoCamerasSystem(ABC):
         returns:
         No return.
         """
+
     @abstractmethod
     def get_grayscale_images(self) -> Tuple[bool, np.ndarray, np.ndarray]:
         """
@@ -43,6 +46,29 @@ class TwoCamerasSystem(ABC):
             - np.ndarray: right grayscale image.
         """
         return
+
+    def get_rgb_images(self) -> Tuple[bool, np.ndarray, np.ndarray]:
+        """
+        Get RGB images for both camera.
+
+        args:
+        No arguments.
+
+        returns:
+        Tuple[bool, np.ndarray, np.ndarray]:
+            - bool: Whether images grabbing is successful or not.
+            - np.ndarray: left RGB image. (BGR in OpenCV format)
+            - np.ndarray: right RGB image. (BGR in OpenCV format)
+        """
+        total_success, left_image1, left_image2 = self.get_grayscale_images()
+        try:
+            left_image1 = cv2.cvtColor(left_image1, cv2.COLOR_GRAY2BGR)
+            left_image2 = cv2.cvtColor(left_image2, cv2.COLOR_GRAY2BGR)
+        except cv2.error:
+            total_success = False
+
+        return total_success, left_image1, left_image2
+
     @abstractmethod
     def get_depth_images(self) -> Tuple[bool, np.ndarray, np.ndarray]:
         """
@@ -58,6 +84,7 @@ class TwoCamerasSystem(ABC):
             - np.ndarray: second depth grayscale image.
         """
         return
+
     @abstractmethod
     def get_width(self) -> int:
         """
@@ -71,6 +98,7 @@ class TwoCamerasSystem(ABC):
             - int: Width of the camera system.
         """
         return
+
     @abstractmethod
     def get_height(self) -> int:
         """
@@ -84,6 +112,7 @@ class TwoCamerasSystem(ABC):
             - int: Height of the camera system.
         """
         return
+
     @abstractmethod
     def release(self) -> bool:
         """
